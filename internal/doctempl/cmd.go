7eea9079d623487f57e83d148d0f8227eb0303a6
hwipl <33433250+hwipl@users.noreply.github.com>
2025-05-25 01:03:50 +0200

    add type DocTemplate
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- internal/doctempl/cmd.go
+++ internal/doctempl/cmd.go
@@ -1,5 +1,31 @@
 package doctempl
 
+import (
+	"io"
+	"text/template"
+)
+
+// DocTemplate is a document template.
+type DocTemplate struct {
+	File     string
+	Template *template.Template
+}
+
+// Execute runs the template with data and writes output to wr.
+func (d *DocTemplate) Execute(wr io.Writer, data any) error {
+	return d.Template.Execute(wr, data)
+}
+
+// NewDocTemplate returns a new DocTemplate.
+func NewDocTemplate(file string) (*DocTemplate, error) {
+	tmpl, err := template.ParseFiles(file)
+	if err != nil {
+		return nil, err
+	}
+
+	return &DocTemplate{file, tmpl}, nil
+}
+
 // Run is the main entry point.
 func Run() {
 }
