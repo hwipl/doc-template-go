a510cdd1c0c746097bf78ae3cd11c1993e514e45
hwipl <33433250+hwipl@users.noreply.github.com>
2025-06-14 18:57:03 +0200

    add test for Config
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

---
+++ internal/doctempl/config_test.go
@@ -0,0 +1,63 @@
+package doctempl
+
+import (
+	"os"
+	"path/filepath"
+	"reflect"
+	"testing"
+)
+
+// TestLoadConfig tests LoadConfig.
+func TestLoadConfig(t *testing.T) {
+	d := t.TempDir()
+
+	// not existing file
+	if _, err := LoadConfig(filepath.Join("does not exist")); err == nil {
+		t.Error("not existing file should return error")
+	}
+
+	// empty file
+	f := filepath.Join(d, "config.json")
+	if err := os.WriteFile(f, []byte(""), 0600); err != nil {
+		t.Fatal(err)
+	}
+	if _, err := LoadConfig(f); err == nil {
+		t.Error("empty file should return error")
+	}
+
+	// valid file
+	data := []byte(`{
+	"Templates": [
+		{
+			"File": "template1.tmpl",
+			"Output": "output1.txt",
+			"DataFile": "datafile1.json"
+		},
+		{
+			"File": "template2.tmpl"
+		}
+	]
+	}`)
+	want := &Config{
+		Templates: []*ConfigTemplate{
+			{
+				File:     "template1.tmpl",
+				Output:   "output1.txt",
+				DataFile: "datafile1.json",
+			},
+			{
+				File: "template2.tmpl",
+			},
+		},
+	}
+	if err := os.WriteFile(f, data, 0600); err != nil {
+		t.Fatal(err)
+	}
+	got, err := LoadConfig(f)
+	if err != nil {
+		t.Error(err)
+	}
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v, want %v", *got, *want)
+	}
+}
